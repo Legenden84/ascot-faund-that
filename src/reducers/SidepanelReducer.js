fa96eff2a8f9cfab1261db94351c594b49b5be0b
Tobias Legenden <tobiaslegenden@gmail.com>
2024-08-03 10:51:11 +0200

    file structure

---
+++ src/reducers/SidepanelReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    state: "test"
+};
+
+const SidepanelReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+export default SidepanelReducer;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-import NavbarReducer from './NavbarReducer';
+import SidepanelReducer from './SidepanelReducer';
 
 const rootReducer = combineReducers({
-    navbar: NavbarReducer,
+    sidepanel: SidepanelReducer,
 });
 
 export default rootReducer;
