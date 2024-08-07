a3dd53b9d6c5016127cbf7df10de7cce3eeb686f
Tobias Legenden <tobiaslegenden@gmail.com>
2024-08-07 21:29:01 +0200

    import working!

--- src/components/StatusBar/HiddenModal.js
+++ src/components/StatusBar/HiddenModal.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import * as XLSX from 'xlsx';
 import './HiddenModal.css';
 
 class HiddenModal extends Component {
     state = {
         password: '',
         isAuthenticated: false,
-        error: ''
+        error: '',
+        selectedFile: null,
     };
 
     handlePasswordChange = (e) => {
@@ -25,6 +27,78 @@ class HiddenModal extends Component {
         if (window.confirm("Are you sure you want to delete all rows? This action cannot be undone.")) {
             this.props.nukeCsv();
             this.props.onClose();
+        }
+    };
+
+    handleFileChange = (e) => {
+        this.setState({ selectedFile: e.target.files[0] });
+    };
+
+    handleImport = async () => {
+        const { selectedFile } = this.state;
+        if (!selectedFile) {
+            alert("Please select a file first.");
+            return;
+        }
+
+        const reader = new FileReader();
+        reader.onload = (event) => {
+            const data = new Uint8Array(event.target.result);
+            const workbook = XLSX.read(data, { type: 'array' });
+            const firstSheetName = workbook.SheetNames[0];
+            const worksheet = workbook.Sheets[firstSheetName];
+            const rows = XLSX.utils.sheet_to_json(worksheet);
+
+            const newCsvData = this.prepareData(rows);
+            this.uploadDataToS3(newCsvData);
+        };
+        reader.readAsArrayBuffer(selectedFile);
+    };
+
+    prepareData = (rows) => {
+        const { csvData } = this.props;
+        const newCsvData = [...csvData];
+
+        rows.forEach((row, index) => {
+            const lastRow = newCsvData.length > 0 ? newCsvData[newCsvData.length - 1] : null;
+            let newId;
+
+            if (lastRow) {
+                const lastId = parseInt(lastRow.ID, 16);
+                newId = '0x' + (lastId + 1).toString(16).toUpperCase().padStart(4, '0');
+            } else {
+                newId = '0x0001';
+            }
+
+            const currentDate = new Date();
+            const formattedDate = `${currentDate.getDate().toString().padStart(2, '0')}-${(currentDate.getMonth() + 1).toString().padStart(2, '0')}-${currentDate.getFullYear()}`;
+
+            const newRow = {
+                ID: newId,
+                room: row.room || '',
+                created: formattedDate,
+                description: row.description || '',
+                phone: row.phone || '',
+                email: row.email || '',
+                deleted: '',
+                'country-code': '',
+                contacted: false,
+                status: '',
+            };
+
+            newCsvData.push(newRow);
+        });
+
+        return newCsvData;
+    };
+
+    uploadDataToS3 = async (data) => {
+        try {
+            await this.props.updateCsv(data);
+            alert('Data successfully uploaded to S3!');
+        } catch (error) {
+            console.error('Error uploading data to S3:', error);
+            alert('Failed to upload data to S3.');
         }
     };
 
@@ -61,6 +135,10 @@ class HiddenModal extends Component {
                                 <p>Password accepted. You can now proceed with the action.</p>
                                 <button className="NukeButton" onClick={this.handleNuke}>
                                     NUKE (Delete All)
+                                </button>
+                                <input type="file" accept=".xlsx, .xls" onChange={this.handleFileChange} />
+                                <button className="ImportButton" onClick={this.handleImport}>
+                                    Import Excel File
                                 </button>
                             </div>
                         )}
--- src/containers/HiddenModalContainer.js
+++ src/containers/HiddenModalContainer.js
@@ -1,9 +1,14 @@
 import { connect } from 'react-redux';
 import HiddenModal from '../components/StatusBar/HiddenModal';
-import { nukeCsv } from '../actions/MainWindowActions';
+import { nukeCsv, updateCsv } from '../actions/MainWindowActions';
+
+const mapStateToProps = (state) => ({
+    csvData: state.aws.csvData,
+});
 
 const mapDispatchToProps = (dispatch) => ({
     nukeCsv: () => dispatch(nukeCsv()),
+    updateCsv: (data) => dispatch(updateCsv(data)),
 });
 
-export default connect(null, mapDispatchToProps)(HiddenModal);
+export default connect(mapStateToProps, mapDispatchToProps)(HiddenModal);
