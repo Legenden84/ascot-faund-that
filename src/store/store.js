c3b8ab59f7a2f12137cdbd8676b2dd79e9002d28
Tobias Legenden <tobiaslegenden@gmail.com>
2024-08-11 17:33:43 +0200

    remove localStorage

--- src/store/store.js
+++ src/store/store.js
@@ -1,23 +1,12 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { thunk } from 'redux-thunk';
-// import { loadState, saveState, clearState } from './localStorage.js';
 import rootReducer from './../reducers/rootReducer.js';
 
-// const persistedState = loadState();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
     rootReducer,
-    // persistedState,
     composeEnhancers(applyMiddleware(thunk))
 );
-
-// store.subscribe(() => {
-//     saveState(store.getState());
-// });
-
-// if (persistedState === undefined) {
-//     clearState();
-// }
 
 export default store;
